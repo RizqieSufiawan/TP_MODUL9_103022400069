0f43e173b2e522d7da14e04596b6347747e619f5
RizqieSufiawan <iamrizqie@gmail.com>
2026-04-27 04:20:19 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TP_MODUL9_103022400069
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
